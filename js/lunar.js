bbab51790fc3b14710d1263c5a27e026b563837e
Laysar <laysar.main@gmail.com>
2025-12-31 01:06:11 -0500

    Added more lunar poses

--- js/lunar.js
+++ js/lunar.js
@@ -8,6 +8,24 @@ export var PoseType;
     PoseType["CROSSED_LEGS"] = "criss_cross";
     PoseType["ULTIMATE"] = "ultimate";
     PoseType["ISOMETRIC"] = "isometric";
+    PoseType["HEAD"] = "head";
+    PoseType["CUSTOM"] = "custom";
+    PoseType["CHEERING"] = "cheering";
+    PoseType["RELAXING"] = "relaxing";
+    PoseType["TRUDGING"] = "trudging";
+    PoseType["COWERING"] = "cowering";
+    PoseType["POINTING"] = "pointing";
+    PoseType["LUNGING"] = "lunging";
+    PoseType["DUNGEONS"] = "dungeons";
+    PoseType["FACEPALM"] = "facepalm";
+    PoseType["SLEEPING"] = "sleeping";
+    PoseType["DEAD"] = "dead";
+    PoseType["ARCHER"] = "archer";
+    PoseType["KICKING"] = "kicking";
+    PoseType["MOJANG_AVATAR"] = "mojavatar";
+    PoseType["READING"] = "reading";
+    PoseType["HIGH_GROUND"] = "high_ground";
+    PoseType["CLOWN"] = "clown";
 })(PoseType || (PoseType = {}));
 export function lunar_api_builder(uuid) {
     return (pose_type) => {
